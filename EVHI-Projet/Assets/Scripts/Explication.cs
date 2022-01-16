49a6a5c4cdee30e9cfa26bdabbbb10d6bb38a70d
EmilieBiegas <48691927+EmilieBiegas@users.noreply.github.com>
2022-01-16 23:32:53 +0100

    Ajout de la fiche d'aide

---
+++ EVHI-Projet/Assets/Scripts/Explication.cs
@@ -0,0 +1,9 @@
+[System.Serializable] 
+// Classe représentant les question / réponses
+public class Explication
+{
+	public string mot;
+    public string nature; //nature de mot possible: "nom","adjectif","verbe","adverbe"
+    public string exemple; //utilisation de mot
+    public string definition;
+}
---
+++ EVHI-Projet/Assets/Scripts/FauxAmi.cs
@@ -0,0 +1,8 @@
+[System.Serializable] 
+// Classe représentant les question / réponses
+public class FauxAmi
+{
+    //public string motcorrecte;//
+    public string fauxami;//
+    public string traduction;
+}
